309f1b87ba6db49389f8d72e852a46dddf123787
Alexsandro Andrade <alexsandro.mpe@gmail.com>
2022-10-14 09:20:04 -0300

    primeiro commit

---
+++ src/test/java/com/alex/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
