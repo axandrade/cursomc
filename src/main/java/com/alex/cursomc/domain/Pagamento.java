d50b3324777c3ea6ec721b875bee0700d9f597cc
Alexsandro Andrade <alexsandro.mpe@gmail.com>
2022-10-15 15:10:22 -0300

    ajustes jsonignore

--- src/main/java/com/alex/cursomc/domain/Pagamento.java
+++ src/main/java/com/alex/cursomc/domain/Pagamento.java
@@ -1,7 +1,6 @@
 package com.alex.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.Objects;
 
 import javax.persistence.Entity;
@@ -15,7 +14,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.alex.cursomc.domain.enums.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -29,7 +28,7 @@ public abstract class Pagamento implements Serializable{
 	
 	private Integer estadoPagamento;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
