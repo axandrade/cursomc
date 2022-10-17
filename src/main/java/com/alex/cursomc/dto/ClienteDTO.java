4780de39d1ef00442530451b8011cf7d21d8ee90
Alexsandro Andrade <alexsandro.mpe@gmail.com>
2022-10-17 16:14:59 -0300

    PUT, DELETE e GET para clientes

---
+++ src/main/java/com/alex/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,64 @@
+package com.alex.cursomc.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.alex.cursomc.domain.Cliente;
+
+public class ClienteDTO {
+
+	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 80 Caracteres")
+	private String nome;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Email(message = "Email Invalido")
+	private String email;
+	
+	
+	public ClienteDTO() {
+	}
+
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+
+}
--- src/main/java/com/alex/cursomc/services/ClienteService.java
+++ src/main/java/com/alex/cursomc/services/ClienteService.java
@@ -1,24 +1,73 @@
 package com.alex.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alex.cursomc.domain.Cliente;
+import com.alex.cursomc.dto.ClienteDTO;
 import com.alex.cursomc.repositories.ClienteRepository;
+import com.alex.cursomc.services.exceptions.DataIntegrityException;
 import com.alex.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
 
 	@Autowired
-	private ClienteRepository categoriaRepository;	
+	private ClienteRepository clienteRepository;	
 
 	public Cliente findById(Integer id) {
-		Optional<Cliente> obj = categoriaRepository.findById(id);
+		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return clienteRepository.findAll(pageRequest);
+
+	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return clienteRepository.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return clienteRepository.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
+		}
+
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 
 }
