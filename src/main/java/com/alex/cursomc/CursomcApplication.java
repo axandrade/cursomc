cca5bb987aeb9dfa7fcbc5756291197aa03ae568
Alexsandro Andrade <alexsandro.mpe@gmail.com>
2022-10-17 06:33:21 -0300

    paginacao com parametros opcionais na requisicao

--- src/main/java/com/alex/cursomc/CursomcApplication.java
+++ src/main/java/com/alex/cursomc/CursomcApplication.java
@@ -68,11 +68,16 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat1 = new Categoria(null, "Informática");		
+		Categoria cat2 = new Categoria(null, "Escritório");		
+		Categoria cat3 = new Categoria(null, "Eletrônicos");
+		Categoria cat4 = new Categoria(null, "Jardinagem");
+		Categoria cat5 = new Categoria(null, "Decoração");
+		Categoria cat6 = new Categoria(null, "Perfumaria");
+		Categoria cat7 = new Categoria(null, "Infantil");
+		Categoria cat8 = new Categoria(null, "Feminino");
+		Categoria cat9 = new Categoria(null, "Automoveis");
 		
-		Categoria cat2 = new Categoria(null, "Escritório");
-		
-		Categoria cat3 = new Categoria(null, "Teste");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		
@@ -87,7 +92,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));		
 		p3.getCategorias().add(cat1);		
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/alex/cursomc/services/CategoriaService.java
+++ src/main/java/com/alex/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alex.cursomc.domain.Categoria;
@@ -23,9 +26,17 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return categoriaRepository.findAll(pageRequest);
 
 	}
 
@@ -47,6 +58,5 @@ public class CategoriaService {
 		}
 
 	}
-	
 
 }
