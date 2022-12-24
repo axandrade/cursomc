87802d5e7c44222135b222589605c36a528280ab
alexsandro andrade <alexsandro.mpe@gmail.com>
2022-12-24 16:07:38 -0300

    ta funfanfo essa porra

--- src/main/java/com/alex/cursomc/config/SecurityConfig.java
+++ src/main/java/com/alex/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.alex.cursomc.security.JWTAuthenticationFilter;
+import com.alex.cursomc.security.JWTAuthorizationFilter;
 import com.alex.cursomc.security.JWTUtil;
 
 @Configuration
@@ -34,32 +35,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
-	private static final String[]  PUBLIC_MATCHERS = {
+	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
-			
 	};
-	
-	private static final String[]  PUBLIC_MATCHERS_GET = {			
-			"/produtos/**",
-			"/categorias/**",
-			"/clientes/**"
+
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",			
+			"/estados/**"
+	};
+
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/clientes/**",
+			"/auth/forgot/**"
 	};
 	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
-			//ATENCAO COM ESSA PARTE
-			http.headers().frameOptions().disable();
-		}
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET ,PUBLIC_MATCHERS_GET).permitAll()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated();
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
