957579bbbb4ee5631b03847940e18c92f9acf49a
alexsandro andrade <alexsandro.mpe@gmail.com>
2022-12-24 14:53:45 -0300

    ate aqui tudo funfando tb

---
+++ src/main/java/com/alex/cursomc/services/exceptions/AuthorizationException.java
@@ -0,0 +1,9 @@
+package com.alex.cursomc.services.exceptions;
+
+public class AuthorizationException extends RuntimeException{
+	
+	public AuthorizationException(String msg) {
+        super(msg);
+    }
+
+}
